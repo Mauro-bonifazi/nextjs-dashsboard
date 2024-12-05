37e57312e0439366be4d5a369f8ca8db1a5c2daa
Mauro Bonifazi <Maurobonifazi@hotmail.com>
2024-12-04 23:24:57 -0300

    first-commit

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import { montserrat, roboto } from "./ui/fonts";
+import "./ui/global.css";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Montserrat, Roboto, Lusitana } from "next/font/google";
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+});
+export const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
